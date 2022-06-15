c2e271c742b3252414c2c393d5357292641b9acd
GARM2021 <ivanhoe.real@gmail.com>
2022-06-15 17:14:25 -0500

    C58 Construccion de Rutas

--- rutas/rutas.php
+++ rutas/rutas.php
@@ -17,42 +17,51 @@ if (($arrRut2) == "") {
     );
 
     echo json_encode($json, true);
+
+    return;
 } else {
 
     /*======================================================================================*/
     //! Cuando se hace peticiones desde registro                                                             
     /*======================================================================================*/
-
-    if ($arrRut2 == "registro") {
-        $json = array(
-            "detalle" => "estoy en registro"
-        );
-
-        echo json_encode($json, true);
-        # code...
-    }
-
-    if ($arrRut2 == "cursos") {
-        $json = array(
-            "detalle" => "estoy en cursos"
-        );
-
-        echo json_encode($json, true);
-        # code...
-    }
-    /*======================================================================================*/
-    //! Cuando se hace peticiones desde un solo curso                                                             
-    /*======================================================================================*/
-    if (count($arrayRutas) > 3) {
-
-        if ($arrRut2 == "cursos" &&  is_numeric(array_filter($arrayRutas)[3])) {
-
+    if (count(array_filter($arrayRutas)) == 2) {
+        if ($arrRut2 == "registro") {
             $json = array(
-                "detalle" => "estoy en un solo  curso"
+                "detalle" => "estoy en registro"
             );
 
             echo json_encode($json, true);
+
+            return;
+            # code...
         }
-     
+
+        if ($arrRut2 == "cursos") {
+            $json = array(
+                "detalle" => "estoy en cursos"
+            );
+
+            echo json_encode($json, true);
+
+            return;
+            # code...
+        }
+    } else {
+        /*======================================================================================*/
+        //! Cuando se hace peticiones desde un solo curso                                                             
+        /*======================================================================================*/
+        if (count($arrayRutas) > 3) {
+
+            if ($arrRut2 == "cursos" &&  is_numeric(array_filter($arrayRutas)[3])) {
+
+                $json = array(
+                    "detalle" => "estoy en un solo  curso"
+                );
+
+                echo json_encode($json, true);
+
+                return;
+            }
+        }
     }
 }
